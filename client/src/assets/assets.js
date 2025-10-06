11cd9fb979ed9c582e042b2961d48530e17eca09
markpatel13 <markpatel044@gmail.com>
2025-10-06 19:31:35 +0530

    msg

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -9,6 +9,7 @@ import sample_img_1 from './sample_img_1.png'
 import sample_img_2 from './sample_img_2.png'
 import profile_img_1 from './profile_img_1.png'
 import profile_img_2 from './profile_img_2.png'
+import profile_img_3 from './profile_img_3.jpg'
 import step_icon_1 from './step_icon_1.svg'
 import step_icon_2 from './step_icon_2.svg'
 import step_icon_3 from './step_icon_3.svg'
@@ -73,9 +74,9 @@ export const testimonialsData = [
         text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
     },
     {
-        image:profile_img_1,
-        name:'Donald Jackman',
-        role:' Graphic Designer',
+        image:profile_img_3,
+        name:'Kiara Taylor',
+        role:'Web Developer',
         stars:5,
         text:`I've been using bg.removal for nearly two years, primarily for Instagram, and it has been incredibly user-friendly, making my work much easier.`
     },
